c3286c2896dfa51b0ed76d4fd812f6fc0236d3d8
globeele <nsetko@yahoo.com>
2023-04-11 16:22:58 +0300

    add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "nastya")
+    {
+        Console.WriteLine("Hey, Anastasia");
+    }
+else 
+    {
+        Console.Write("Hello, ");
+        Console.WriteLine(username);
+    }
